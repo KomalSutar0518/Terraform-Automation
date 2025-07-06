2e0b45a11a8db9ec78a3125637d79934751e5fd2
KomalSutar0518 <158801107+KomalSutar0518@users.noreply.github.com>
2025-07-06 10:49:57 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-s3-buckets-forterra-2"
+    bucket = "s3-bucket-for-terraform-1"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
